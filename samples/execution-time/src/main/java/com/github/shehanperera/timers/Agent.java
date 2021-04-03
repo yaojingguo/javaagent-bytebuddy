6b3a15f277c6d327887c110f90a837dc1a1ab025
Jingguo Yao <yaojingguo@gmail.com>
2021-04-03 09:37:15 +0800

    update

--- samples/execution-time/src/main/java/com/github/shehanperera/timers/Agent.java
+++ samples/execution-time/src/main/java/com/github/shehanperera/timers/Agent.java
@@ -1,28 +1,24 @@
 package com.github.shehanperera.timers;
 
+import com.github.shehanperera.util.Util;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
 
 import java.lang.instrument.Instrumentation;
 
-/**
- * This is an agent for get execution time of all running methods
- */
+/** This is an agent for get execution time of all running methods */
 public class Agent {
 
-    public static void premain(String arguments, Instrumentation instrumentation) {
+  public static void premain(String arguments, Instrumentation instrumentation) {
+    Util.println("Agent for time measure");
 
-        System.out.println("Agent for time measure");
-
-        new AgentBuilder.Default()
-                .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
-                .type((ElementMatchers.any()))
-                .transform((builder, typeDescription, classLoader, module) -> builder
-                        .method(ElementMatchers.any())
-                        .intercept(Advice.to(TimerAdvice.class))
-                ).installOn(instrumentation);
-
-    }
-
+    new AgentBuilder.Default()
+        .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
+        .type((ElementMatchers.any()))
+        .transform(
+            (builder, typeDescription, classLoader, module) ->
+                builder.method(ElementMatchers.any()).intercept(Advice.to(TimerAdvice.class)))
+        .installOn(instrumentation);
+  }
 }
--- samples/execution-time/src/main/java/com/github/shehanperera/timers/TimerAdvice.java
+++ samples/execution-time/src/main/java/com/github/shehanperera/timers/TimerAdvice.java
@@ -3,22 +3,19 @@ package com.github.shehanperera.timers;
 import net.bytebuddy.asm.Advice;
 
 public class TimerAdvice {
-    /**
-     * From this  enter method we start the timer and pass that value to exit method and the we getting the time
-     * speed for each method
-     */
-    @Advice.OnMethodEnter
-    static long enter(@Advice.Origin String method) throws Exception {
+  /**
+   * From this enter method we start the timer and pass that value to exit method and the we getting
+   * the time speed for each method
+   */
+  @Advice.OnMethodEnter
+  static long enter(@Advice.Origin String method) throws Exception {
+    long start = System.currentTimeMillis();
+    return start;
+  }
 
-        long start = System.currentTimeMillis();
-        return start;
-    }
-
-    @Advice.OnMethodExit
-    static void exit(@Advice.Origin String method, @Advice.Enter long start) throws Exception {
-
-        long end = System.currentTimeMillis();
-        System.out.println(method + " took " + (end - start) + " milliseconds ");
-    }
-
+  @Advice.OnMethodExit
+  static void exit(@Advice.Origin String method, @Advice.Enter long start) throws Exception {
+    long end = System.currentTimeMillis();
+    System.out.println(method + " took " + (end - start) + " milliseconds ");
+  }
 }
