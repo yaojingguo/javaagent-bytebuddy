c37f7d2d3d2d306fa5c217ce85e41846e9338709
Jingguo Yao <yaojingguo@gmail.com>
2021-04-03 22:08:33 +0800

    update

--- samples/get-methods/src/main/java/com/github/shehanperera/allmethods/AllMethod.java
+++ samples/get-methods/src/main/java/com/github/shehanperera/allmethods/AllMethod.java
@@ -1,13 +1,12 @@
 package com.github.shehanperera.allmethods;
 
+import com.github.shehanperera.util.Util;
 import net.bytebuddy.asm.Advice;
 
 public class AllMethod {
 
-    @Advice.OnMethodExit
-    static void getAllMethods(@Advice.Origin String method) throws Exception {
-
-        System.out.println(method);
-    }
-
+  @Advice.OnMethodExit
+  static void getAllMethods(@Advice.Origin String method) throws Exception {
+    Util.println(method);
+  }
 }
