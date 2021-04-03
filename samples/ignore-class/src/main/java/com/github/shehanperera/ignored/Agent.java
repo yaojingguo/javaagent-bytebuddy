bd3ab9a301f0149db88aedff20819e45f4254a35
Jingguo Yao <yaojingguo@gmail.com>
2021-04-03 22:23:04 +0800

    update

--- samples/ignore-class/src/main/java/com/github/shehanperera/ignored/Agent.java
+++ samples/ignore-class/src/main/java/com/github/shehanperera/ignored/Agent.java
@@ -1,5 +1,6 @@
 package com.github.shehanperera.ignored;
 
+import com.github.shehanperera.util.Util;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
@@ -7,24 +8,22 @@ import net.bytebuddy.matcher.ElementMatchers;
 import java.lang.instrument.Instrumentation;
 
 /**
- * This is a agent for instrument class that ignored by byte-buddy. Normally byte-buddy ignore java class.
- * From .ignore(ElementMatchers.none()) we can get all running class in floor and we can use .visit for get more
- * details of methods and constructors in these ignored class
+ * This is a agent for instrument class that ignored by byte-buddy. Normally byte-buddy ignore java
+ * class. From .ignore(ElementMatchers.none()) we can get all running class in floor and we can use
+ * .visit for get more details of methods and constructors in these ignored class
  */
 public class Agent {
 
-    public static void premain(String arguments, Instrumentation instrumentation) {
+  public static void premain(String arguments, Instrumentation instrumentation) {
+    Util.println("Agent for instrument ignored class like java class ");
 
-        System.out.println("Agent for instrument ignored class like java class ");
-
-        new AgentBuilder.Default()
-                .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
-                .ignore(ElementMatchers.none())
-                .type((ElementMatchers.any()))
-                .transform((builder, typeDescription, classLoader, module) -> builder
-                        .method(ElementMatchers.any())
-                        .intercept(Advice.to(IgnoreClass.class))
-                ).installOn(instrumentation);
-
-    }
+    new AgentBuilder.Default()
+        .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
+        .ignore(ElementMatchers.none())
+        .type((ElementMatchers.any()))
+        .transform(
+            (builder, typeDescription, classLoader, module) ->
+                builder.method(ElementMatchers.any()).intercept(Advice.to(IgnoreClass.class)))
+        .installOn(instrumentation);
+  }
 }
--- samples/ignore-class/src/main/java/com/github/shehanperera/ignored/IgnoreClass.java
+++ samples/ignore-class/src/main/java/com/github/shehanperera/ignored/IgnoreClass.java
@@ -1,14 +1,11 @@
 package com.github.shehanperera.ignored;
 
+import com.github.shehanperera.util.Util;
 import net.bytebuddy.asm.Advice;
 
 public class IgnoreClass {
-
-    @Advice.OnMethodExit
-    public static void enterMethods(@Advice.Origin String method) throws Exception {
-
-        System.out.println("method "+ method);
-
-    }
-
+  @Advice.OnMethodExit
+  public static void enterMethods(@Advice.Origin String method) throws Exception {
+    Util.println("method: " + method);
+  }
 }
